4d8e1df17cf54b825722bb34f24d1d8313b1b564
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2021-01-14 15:02:55 -0500

    sendRedirect DemoApp4

---
+++ DemoApp4/src/com/adder/AddServlet.java
@@ -0,0 +1,38 @@
+package com.adder;
+
+/* simple servlet program which takes 2 numbers from user from webpage using HttpServletRequest and make the sum of those 2 number
+ * and send it to another servlet using SendRedirect. Here we Rewrite the url and send it to the browser. Browser calls the SqServlet
+ * by passing the k value from url which we rewrote.
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+	// HttpServletRequest and HttpServletResponse  are both interface, Tomcat does the implementation and returns its object to us, 
+	// so we can use them.
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+		// req.getParameter("num1") returns string here so we use method to change it to integer
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int k = i + j;
+
+		/* We can transfer data from one servlet to another servlet under sendRedirect using 3 ways-
+		 * 1. Session  2. Cookies  3. Url ReWriting
+		*/
+		
+		// Here we are using Url Rewriting
+		// here we are sending k as in it would get sent in doGet request to browser which calls for another servlet.
+		resp.sendRedirect("sq?k="+k);
+		
+	}
+	
+
+}
---
+++ DemoApp4/src/com/adder/SqServlet.java
@@ -0,0 +1,26 @@
+package com.adder;
+
+// Servlet AddServlet sends the request url to browser and browser calls the Servlet SqServlet
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		int k = Integer.parseInt(req.getParameter("k"));
+		
+		k = k*k;
+		
+		// getWriter() returns an object of PrintWriter
+		PrintWriter out = resp.getWriter();
+		out.println("From SqServlet(calling servlet from servlet) using SendRedirect, the square of the sum of the given two number is \n"+k );
+		
+	}
+}
